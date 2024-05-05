9f79e52d357c4130ad357240e5d7111794b882e2
asnyd014 <91281848+asnyd014@users.noreply.github.com>
2024-05-05 14:06:00 -0700

    Fix errors and properly use priority queue

--- Problem.h
+++ Problem.h
@@ -18,20 +18,27 @@ struct SolutionNode {
     int h_n;
 };
 
+struct CompareNode {
+    bool operator() (SolutionNode const& n1, SolutionNode const& n2) {
+        return (n1.g_n + n1.h_n) + (n2.g_n + n2.h_n);
+    }
+};
+
 class Problem {
 
         // 3 columns and 3 rows for normal 8 puzzle
-        static int probColumns = 3;
-        static int probRows = 3;
+        static const int probColumns = 3;
+        static const int probRows = 3;
 
-        std::vector<int> initialState(probColumns * probRows);
-        std::vector<int> goalState(probColumns * probRows);
+        std::vector<int> initialState;
+        std::vector<int> goalState;
         algorithm heuristic;
 
     private:
         void calcHeuristic(SolutionNode& newNode) {
             // Set new node's g(n) to parent's g(n) + 1
-            newNode.g_n = newNode.(*parent).g_n + 1; 
+            SolutionNode* parent = newNode.parent;
+            newNode.g_n = (*parent).g_n + 1; 
 
             // Calculate h(n) based on algorithm chosen
             switch (this->heuristic) {
@@ -56,7 +63,7 @@ class Problem {
 
         // Operators
         // These will check validity of operations and only add valid nodes to the provided frontier
-        void blankLeft(SolutionNode& currNode, std::priority_queue& frontier, blankIndex) {
+        void blankLeft(SolutionNode& currNode, std::priority_queue<SolutionNode, std::vector<SolutionNode>, CompareNode>& frontier, int blankIndex) {
             if ((blankIndex + 1) % probColumns != 1) { // Check if blank is already on left edge
                 // Create new node
                 SolutionNode newNode;
@@ -74,7 +81,7 @@ class Problem {
             }
         }
 
-        void blankRight(SolutionNode& currNode, std::priority_queue& frontier, blankIndex) {
+        void blankRight(SolutionNode& currNode, std::priority_queue<SolutionNode, std::vector<SolutionNode>, CompareNode>& frontier, int blankIndex) {
             if ((blankIndex + 1) % probColumns != 0) { // Check if blank is already on right edge
                 // Create new node
                 SolutionNode newNode;
@@ -92,7 +99,7 @@ class Problem {
             }
         }
 
-        void blankUp(SolutionNode& currNode, std::priority_queue& frontier, blankIndex) {
+        void blankUp(SolutionNode& currNode, std::priority_queue<SolutionNode, std::vector<SolutionNode>, CompareNode>& frontier, int blankIndex) {
             if ((blankIndex + 1) > probColumns) { // Check if blank is already on top edge
                 // Create new node
                 SolutionNode newNode;
@@ -110,7 +117,7 @@ class Problem {
             }
         }
 
-        void blankDown(SolutionNode& currNode, std::priority_queue& frontier, blankIndex) {
+        void blankDown(SolutionNode& currNode, std::priority_queue<SolutionNode, std::vector<SolutionNode>, CompareNode>& frontier, int blankIndex) {
             if ((blankIndex + 1) <= (probColumns * (probRows - 1))) { // Check if blank is already on bottom edge
                 // Create new node
                 SolutionNode newNode;
@@ -133,8 +140,10 @@ class Problem {
         Problem(std::vector<int> initialState, algorithm heuristic) { 
             this->initialState = initialState; 
             this->heuristic = heuristic; 
-            
+
             // Fill goal state with sequential values starting with 1 and ending with 0
+            int totalSpace = probColumns * probRows;
+            goalState.reserve(totalSpace);
             std::iota(goalState.begin(), goalState.end()-1, 1);
             goalState[goalState.size()-1] = 0;
         }
@@ -145,7 +154,7 @@ class Problem {
 
         // Expansion Function
         // To be called from driver code
-        void expandNode(SolutionNode& currNode, std::priority_queue& frontier) {
+        void expandNode(SolutionNode& currNode, std::priority_queue<SolutionNode, std::vector<SolutionNode>, CompareNode>& frontier) {
             // Find index of blank space
             int blankIndex = 0;
             for (int i = 0; i < currNode.state.size(); ++i) {
