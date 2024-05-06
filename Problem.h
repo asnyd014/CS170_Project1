738b045c013f70875116ddd8829f47196f0bcef7
asnyd014 <91281848+asnyd014@users.noreply.github.com>
2024-05-05 22:25:06 -0700

    Implement A* with Euclidean Distance heuristic

--- Problem.h
+++ Problem.h
@@ -17,8 +17,8 @@ enum algorithm { UCS, AMT, AED };
 struct SolutionNode {
     std::vector<int> state;
     SolutionNode* parent = nullptr;
-    int g_n;
-    int h_n;
+    float g_n;
+    float h_n;
 
     bool operator() (const SolutionNode& n1, const SolutionNode& n2) const {
         return (n1.g_n + n1.h_n) > (n2.g_n + n2.h_n);
@@ -54,15 +54,43 @@ class Problem {
                     // Check if values at each index of the current state and the goal state match
                     // If they do not, increment number of misplaced tiles by one
                     newNode.h_n = 0;
-                    for (int i = 0; i < newNode.state.size(); ++i) {
+                    for (int i = 0; i < goalState.size(); ++i) {
                         if (newNode.state[i] != goalState[i]) { ++newNode.h_n; }
                     }
                     break;
                 case AED: // A* with Euclidean Distance
-                    
-                    /
+                {    
+                    int x1, x2, y1, y2 = 0;
+                    int targetIndex = 0;
+                    newNode.h_n = 0;
 
+                    // Iterate through goal state, determine distance each value is from corresponding value in node state
+                    for (int i = 0; i < goalState.size(); ++i) {
+                        // Translate current goal state index into coordinates
+                        x1 = i % probColumns;
+                        y1 = (i / probColumns) * -1;
+
+                        // Check for matching value in current state
+                        for (int j = 0; j < goalState.size(); ++j) {
+                            if (newNode.state[j] == goalState[i]) { targetIndex = j; break; }
+                        }
+
+                        if (i != targetIndex) {
+
+                            // Translate current goal state index into coordinates
+                            x1 = i % probColumns;
+                            y1 = (i / probColumns) * -1;
+                        
+                            // Translate current node state index into coordinates
+                            x2 = (targetIndex % probColumns);
+                            y2 = (targetIndex / probColumns) * -1;
+
+                            // Calculate distance and add to h(n)
+                            newNode.h_n += sqrt(pow((x2-x1), 2) + pow((y2-y1), 2));
+                        }
+                    }
                     break;
+                }
                 default: // Just apply UCS by default
                     newNode.h_n = 0;
                     break;
